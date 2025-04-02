a34a9310fc77cac6139a0a86f658302cce7a0b4e
Andreas Nyberg <andreas.nyberg@chas.se>
2025-04-02 20:45:42 +0200

    Adds styling & states

--- app/ui/todo.tsx
+++ app/ui/todo.tsx
@@ -1,19 +1,55 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Todo() {
+  const [checked, setChecked] = useState(false);
+  const [inEditMode, setInEditMode] = useState(false);
+
+  function toggleCheck() {
+    setChecked(!checked);
+  }
+
   return (
-    <label className="list-group-item d-flex gap-3 justify-between items-center">
-      <div>
-        <input type="checkbox" className="form-check-input flex-shrink-0" />
-        <span className="pt-1 form-checked-content ml-3">
-          <strong>hej</strong>
-        </span>
+    <label className="list-group-item d-flex gap-3 justify-between items-center !py-0 h-14">
+      <div className="flex items-center gap-x-3">
+        <input
+          type="checkbox"
+          className="form-check-input flex-shrink-0"
+          checked={checked}
+          onChange={toggleCheck}
+        />
+
+        {inEditMode ? (
+          <input type="text" className="form-control inline" />
+        ) : (
+          <span className="pt-1 form-checked-content">
+            <strong>hej</strong>
+          </span>
+        )}
       </div>
 
-      <div className="flex gap-3">
-        <button className="btn btn-sm btn-primary">edit</button>
-        <button className="btn btn-sm btn-danger">X</button>
+      <div className="flex gap-3 items-center">
+        {inEditMode ? (
+          <button
+            className="btn btn-sm h-7 !text-xs tracking-widest !font-bold btn-success"
+            onClick={() => setInEditMode(false)}
+          >
+            OK
+          </button>
+        ) : (
+          <button
+            className="btn btn-sm h-7 !text-xs tracking-widest !font-bold btn-secondary"
+            onClick={() => setInEditMode(true)}
+          >
+            EDIT
+          </button>
+        )}
+
+        <button className="btn btn-sm btn-danger h-7 !text-xs tracking-widest !font-bold">
+          X
+        </button>
       </div>
-      {/* <input type="text" className="form-control" /> */}
-      {/* <button className="w-100 btn btn-primary btn-lg mt-4">ADD</button> */}
     </label>
   );
 }
--- app/ui/todos.tsx
+++ app/ui/todos.tsx
@@ -2,11 +2,15 @@ import Todo from "./todo";
 
 export default function Todos() {
   return (
-    <div className="list-group">
-      <Todo />
-      <Todo />
-      <Todo />
-      <Todo />
-    </div>
+    <>
+      <div className="list-group">
+        <Todo />
+        <Todo />
+        <Todo />
+        <Todo />
+      </div>
+
+      <button className="w-100 btn btn-primary btn-lg mt-4">ADD NEW</button>
+    </>
   );
 }
